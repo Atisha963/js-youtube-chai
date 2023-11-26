129227e092e825f8d4277a5a3ee958c717250676
Atisha963 <51511589+Atisha963@users.noreply.github.com>
2023-11-26 09:02:16 +0000

    datatypes and variables done

---
+++ 01_Basics.js/dataTypes.js
@@ -0,0 +1,17 @@
+"use strict" //treat all code as newer js version
+
+let state = "bengaluru"
+let age = 21
+let isLoggedIn = true 
+
+console.log(state)
+
+/*
+null --> Object and is standalone value 
+
+undefined -->  undefined datatype
+
+*/
+
+console.log(typeof age)
+console.log(typeof null)
---
+++ 01_Basics.js/variables.js
@@ -0,0 +1,12 @@
+const accountName = "atisha"
+let accountId = "12311"
+let accountCity= "Bengaluru"
+var accountState;
+
+/*
+don't use var 
+bcz of block and functional scope
+
+*/
+console.log(accountId)
+console.table([accountCity,accountName, accountId, accountState])
